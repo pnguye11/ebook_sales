2f1abe5015eca1f9f72dba3d89aa3a2bdacca0e1
Nathan Nguyen <pnguye11@gmail.com>
2017-10-12 09:38:37 -0700

    localhost setup and nodemon

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-conts express = require('express');
+const express = require('express');
+const stripe = require('stripe')('pk_test_G3zChCqOlDnDRdKGnCbsmJuD');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+/// start the app
+const app = express();
+
+//// deployment and localhost
+const port = process.env.PORT || 5000;
+
+/// start server.
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
