813166e0a77b9efe39b857af4ed766c4bfeada90
Nathan Nguyen <pnguye11@gmail.com>
2017-10-12 09:58:58 -0700

    upload img

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ const stripe = require('stripe')('pk_test_G3zChCqOlDnDRdKGnCbsmJuD');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 
+
 /// start the app
 const app = express();
+
+//// handlebars middleware
+app.engine('handlebars, exphbs({defaultLayout: 'main'})');
+app.set('view engine, handlebars');
+
+//body parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extened:false}));
+
+/// set static folder
+
+app.use(express.static(`${__dirname}/public`));
+// index Routes
+
+app.get('/', (req, res) =>{
+  res.render('index');
+})
+
 
 //// deployment and localhost
 const port = process.env.PORT || 5000;
